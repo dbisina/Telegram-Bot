d0f920c483ed739e3f7e56ea8352617866d15296
Lyon98.dbios <danbis664@gmail.com>
2022-06-27 22:35:12 +0100

    Update Telegrambot.py

--- Telegrambot.py
+++ Telegrambot.py
@@ -5,12 +5,12 @@ from telegram.ext import *
 
 
 #Fetching the bot information
-bot = Bot("Pass token ID")
+bot = Bot("Pass Token Id")
 print(bot.get_me())
 
 
 #Update for future changes
-updater=Updater("Pass token ID ",use_context=True)
+updater=Updater("Pass Token Id",use_context=True)
 #use_context : if your telegram version is low then use false else use true
 
 #A dispatcher is used to dispatch this update to our telegram bot
@@ -21,7 +21,7 @@ dispatcher=updater.dispatcher
 def test_fnc2(update: Update, context: CallbackContext):
     bot.send_message(
         chat_id=update.effective_chat.id,
-        text="Welcome to the world of Otakus ",
+        text="Welcome to the world of Otakus \n Commands: \n animelist : shows list of anime \n python: github repo ",
         )
 start_value2=CommandHandler('start', test_fnc2)
 dispatcher.add_handler (start_value2)
@@ -34,6 +34,15 @@ def test_fnc(update: Update, context: CallbackContext):
         )
 start_value=CommandHandler('python', test_fnc)
 dispatcher.add_handler (start_value)
+
+def test_fnc(update: Update, context: CallbackContext):
+    bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="anime list: https://kainime.com/anime-lists/",
+        )
+start_value=CommandHandler('animelist', test_fnc)
+dispatcher.add_handler (start_value)
+
 
 #Polling The Update
 updater.start_polling()
